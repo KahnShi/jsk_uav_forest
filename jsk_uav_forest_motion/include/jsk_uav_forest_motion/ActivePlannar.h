2b0f6cfc52d4f71496a087c6633ef6e6a79115df
KahnShi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2017-05-02 14:46:19 +0900

    Add triangle mesh generator from laser_cluster topic.

--- jsk_uav_forest_motion/include/jsk_uav_forest_motion/ActivePlannar.h
+++ jsk_uav_forest_motion/include/jsk_uav_forest_motion/ActivePlannar.h
@@ -9,8 +9,12 @@
 #include <nav_msgs/Odometry.h>
 #include <std_msgs/ColorRGBA.h>
 #include <visualization_msgs/Marker.h>
+#include <sensor_msgs/LaserScan.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <eigen3/Eigen/Core>
+#include <tf/transform_broadcaster.h>
+#include <math.h>
 
 class ActivePlannar
 {
@@ -20,11 +24,21 @@ public:
   bool m_active_plannar_start_flag;
   nav_msgs::Odometry m_uav_odom;
   std::vector<std::vector<geometry_msgs::Point32> > m_triangle_mesh;
+  tf::Vector3 m_uav_pos;
+  tf::Vector3 m_uav_ang;
+  tf::Vector3 m_target_pos;
+  tf::Vector3 m_start_pos;
+  tf::Vector3 m_start_vel;
+  tf::Vector3 m_target_vel;
+  double m_acc_ub;
+  double m_vel_ub;
+  double m_control_period;
 
   /* Subscriber */
   ros::Subscriber m_sub_target_poses;
   ros::Subscriber m_sub_uav_odom;
   ros::Subscriber m_sub_start_flag;
+  ros::Subscriber m_sub_scan_cluster;
 
   /* Publisher */
   ros::Publisher m_pub_control_points;
@@ -34,7 +48,11 @@ public:
   void startFlagCallback(const std_msgs::Empty msg);
   void uavOdomCallback(const nav_msgs::OdometryConstPtr& msg);
   void targetPosesCallback(const geometry_msgs::PolygonStampedConstPtr& msg);
+  void scanClusterCallback(const sensor_msgs::LaserScanConstPtr& msg);
+  void controlCallback(const ros::TimerEvent&);
   void generateTriangleFromPose(const geometry_msgs::PolygonStampedConstPtr& msg, std::vector<std::vector<geometry_msgs::Point32> >& origin_triangle);
+  geometry_msgs::Point32 getGlobalPointFromLaser(double ang, double distance);
+  void visualizeTriangleMesh();
   void point32ToPoint(geometry_msgs::Point32 pt32, geometry_msgs::Point& pt);
 };
 
--- jsk_uav_forest_motion/src/ActivePlannar.cpp
+++ jsk_uav_forest_motion/src/ActivePlannar.cpp
@@ -1,9 +1,16 @@
 #include <jsk_uav_forest_motion/ActivePlannar.h>
 
 void ActivePlannar::onInit(){
+  ros::NodeHandle private_nh("~");
+  private_nh.param("control_period", m_control_period, 0.9);
+  private_nh.param("velocity_upper_bound", m_vel_ub, 7.0);
+  private_nh.param("acceleration_upper_bound", m_acc_ub, 2.0);
+
   m_sub_start_flag = m_nh.subscribe<std_msgs::Empty>("active_plannar_start_flag", 1, &ActivePlannar::startFlagCallback, this);
   m_sub_uav_odom = m_nh.subscribe<nav_msgs::Odometry>("ground_truth/state", 1, &ActivePlannar::uavOdomCallback, this);
   m_sub_target_poses = m_nh.subscribe<geometry_msgs::PolygonStamped>("target_tree_poses", 1, &ActivePlannar::targetPosesCallback, this);
+  m_sub_scan_cluster = m_nh.subscribe<sensor_msgs::LaserScan>("scan_clustered", 1, &ActivePlannar::scanClusterCallback, this);
+  ros::Timer timer = m_nh.createTimer(ros::Duration(m_control_period), &ActivePlannar::controlCallback, this);
 
   m_pub_control_points = m_nh.advertise<geometry_msgs::PolygonStamped>(std::string("control_points"), 1);
   m_pub_triangle_mesh = m_nh.advertise<visualization_msgs::Marker>(std::string("triangle_mesh"), 1);
@@ -11,20 +18,40 @@ void ActivePlannar::onInit(){
   m_active_plannar_start_flag = false;
 }
 
+void ActivePlannar::controlCallback(const ros::TimerEvent&)
+{
+  
+}
+
 void ActivePlannar::startFlagCallback(const std_msgs::Empty msg){
   m_active_plannar_start_flag = true;
 }
 
-void ActivePlannar::uavOdomCallback(const nav_msgs::OdometryConstPtr& msg){
-  m_uav_odom = *msg;
+void ActivePlannar::uavOdomCallback(const nav_msgs::OdometryConstPtr& uav_msg){
+  m_uav_odom = *uav_msg;
+  tf::Quaternion uav_q(uav_msg->pose.pose.orientation.x,
+                       uav_msg->pose.pose.orientation.y,
+                       uav_msg->pose.pose.orientation.z,
+                       uav_msg->pose.pose.orientation.w);
+  tf::Matrix3x3  uav_orientation(uav_q);
+  tfScalar r,p,y;
+  uav_orientation.getRPY(r, p, y);
+  m_uav_ang.setValue(r, p, y);
+  m_uav_pos.setValue(uav_msg->pose.pose.position.x,
+                     uav_msg->pose.pose.position.y,
+                     uav_msg->pose.pose.position.z);
 }
 
 void ActivePlannar::targetPosesCallback(const geometry_msgs::PolygonStampedConstPtr& msg){
-  std::vector<std::vector<geometry_msgs::Point32> > origin_triangle;
-  generateTriangleFromPose(msg, origin_triangle);
+  // get triangle mesh directly from obstacle's ground truth position
+  //generateTriangleFromPose(msg, m_triangle_mesh);
+  //visualizeTriangleMesh();
+}
 
+void ActivePlannar::visualizeTriangleMesh(){
   visualization_msgs::Marker triangle_list_marker;
-  triangle_list_marker.header = msg->header;
+  triangle_list_marker.header.frame_id = "world";
+  triangle_list_marker.header.stamp = ros::Time::now();
   triangle_list_marker.ns = std::string("triangle_mesh");
   triangle_list_marker.action = visualization_msgs::Marker::ADD;
   triangle_list_marker.type = visualization_msgs::Marker::TRIANGLE_LIST;
@@ -37,19 +64,67 @@ void ActivePlannar::targetPosesCallback(const geometry_msgs::PolygonStampedConst
   triangle_list_marker.color.a = 1.0;
   triangle_list_marker.id = 0;
   std_msgs::ColorRGBA color_r;
-  color_r.r = 1.0; color_r.g = 0.0; color_r.b = 0.0; color_r.a = 1.0;
+  color_r.r = 0.0; color_r.g = 0.0; color_r.b = 1.0; color_r.a = 1.0;
   srand (time(NULL));
-  for (int i = 0; i < origin_triangle.size(); ++i){
-    point32ToPoint(origin_triangle[i][0], pt);
+  for (int i = 0; i < m_triangle_mesh.size(); ++i){
+    point32ToPoint(m_triangle_mesh[i][0], pt);
     triangle_list_marker.points.push_back(pt);
-    point32ToPoint(origin_triangle[i][1], pt);
+    point32ToPoint(m_triangle_mesh[i][1], pt);
     triangle_list_marker.points.push_back(pt);
-    point32ToPoint(origin_triangle[i][2], pt);
+    point32ToPoint(m_triangle_mesh[i][2], pt);
     triangle_list_marker.points.push_back(pt);
-    color_r.r = rand() / (double)RAND_MAX * 1.0;
+    color_r.b = rand() / (double)RAND_MAX * 1.0;
     triangle_list_marker.colors.push_back(color_r);
   }
   m_pub_triangle_mesh.publish(triangle_list_marker);
+}
+
+void ActivePlannar::scanClusterCallback(const sensor_msgs::LaserScanConstPtr& laser_msg){
+  m_triangle_mesh.clear();
+  std::vector<geometry_msgs::Point32> p;
+  int start_id, end_id, mid_id;
+  int triangle_num = 0;
+  for (size_t i = 0; i < laser_msg->ranges.size(); i++){
+    if (!std::isnan(laser_msg->ranges[i])){
+      p.push_back(getGlobalPointFromLaser(i*laser_msg->angle_increment+laser_msg->angle_min, fabs(laser_msg->ranges[i])));
+      start_id = i;
+      ++i;
+      while (i < laser_msg->ranges.size()){
+        if (std::isnan(laser_msg->ranges[i])){
+          p.push_back(getGlobalPointFromLaser((i-1)*laser_msg->angle_increment+laser_msg->angle_min, fabs(laser_msg->ranges[i-1])));
+          end_id = i - 1;
+          if (start_id + 2 <= end_id)
+            mid_id = (start_id + end_id) / 2;
+          else
+            mid_id = start_id;
+          p.push_back(getGlobalPointFromLaser((mid_id-1)*laser_msg->angle_increment+laser_msg->angle_min, fabs(laser_msg->ranges[mid_id])));
+          // judge whether cluster is the ground instead of obstacle
+          double range_finder_max_distance = m_uav_pos.z() / (sqrt(1 - pow(cos(m_uav_ang[0]) * cos(m_uav_ang[1]), 2)));
+          // if rangefinder data is very close (eg. <= 1.5m) to its maxinum, this data is likely to be ground's data, it should be abandoned
+          if (fabs(range_finder_max_distance) - fabs(laser_msg->ranges[mid_id]) > 1.5){
+            triangle_num += 1;
+            m_triangle_mesh.push_back(p);
+          }
+          p.clear();
+          break;
+        }
+        ++i;
+      }
+    }
+  }
+  // todo: currently from ground truth, there are 3 triangles. If more than 3, means having wrong detection.
+  if (triangle_num > 3)
+    ROS_WARN("Wrong obstacles are detected: %d", triangle_num - 3);
+  visualizeTriangleMesh();
+}
+
+geometry_msgs::Point32 ActivePlannar::getGlobalPointFromLaser(double ang, double distance){
+  double rot_ang = ang + m_uav_ang[2];
+  geometry_msgs::Point32 pt32;
+  pt32.x = distance * cos(rot_ang) * cos(m_uav_ang[0]) * cos(m_uav_ang[1]) + m_uav_pos.x();
+  pt32.y = distance * sin(rot_ang) * cos(m_uav_ang[0]) * cos(m_uav_ang[1]) + m_uav_pos.y();
+  pt32.z = m_uav_pos.z();
+  return pt32;
 }
 
 void ActivePlannar::generateTriangleFromPose(const geometry_msgs::PolygonStampedConstPtr& msg, std::vector<std::vector<geometry_msgs::Point32> >& origin_triangle){
